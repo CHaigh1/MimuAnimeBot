591518714241e4ed6fcbbc52199d81dc40cdff53
Chris Haigh <christopher@thehaighs.net>
2023-09-06 14:56:27 -0400

    Update animefix.js

--- jsbackup/animefix.js
+++ jsbackup/animefix.js
@@ -1,5 +1,5 @@
 /// animefix.js
-(function(){
+(function animefix(){
   console.log('Hey, does this show up in the console?')
   $('.download-btn').show('slow');
   $('.downloadbtn').attr('disabled', false);
